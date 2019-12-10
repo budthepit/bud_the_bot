853315fc46f053599b18fae7d4273bf1628af2fb
BudThePit <budthepit@hotmail.com>
2019-12-10 10:48:31 -0300

    Added ENV port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, ()=> {
     console.log("Listening on port " + port );
 });
